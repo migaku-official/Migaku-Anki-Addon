a9c977dccc171dc921143d8fb919a07d1fedf8aa
Bent <bent@mail.de>
2021-11-12 22:08:27 +0100

    Added remove syntax dialog

--- src/browser.py
+++ src/browser.py
@@ -9,26 +9,54 @@ from . import util
 from .migaku_connection import ConnectionStatusLabel
 
 
-class AddSyntaxDialog(QDialog):
+class AddRemoveSyntaxDialog(QDialog):
 
     BATCH_SIZE = 100
 
-    def __init__(self, lang: Language, note_ids, parent=None):
+    INITIAL_SIZE = (430, 370)
+
+    class RemoveThread(QThread):
+        finished = pyqtSignal()
+        progress = pyqtSignal(int)
+        def __init__(self, lang: Language, note_ids, field_names, parent=None):
+            super().__init__(parent)
+            self.lang = lang
+            self.note_ids = note_ids
+            self.field_names = field_names
+
+        def run(self):
+            for i, note_id in enumerate(self.note_ids):
+                note = aqt.mw.col.get_note(note_id)
+                if note:
+                    for field_name in self.field_names:
+                        text = self.lang.remove_syntax(note[field_name])
+                        note[field_name] = text
+                    note.flush()
+                if i % AddRemoveSyntaxDialog.BATCH_SIZE == 0:
+                    self.progress.emit(i)
+            self.finished.emit()
+
+    def __init__(self, lang: Language, is_remove: bool, note_ids, parent=None):
         super().__init__(parent)
         self.lang = lang
+        self.is_remove = is_remove
         self.note_ids = note_ids
+        self.note_count = len(self.note_ids)
+        self.progress_string = ('Removing' if is_remove else 'Adding') + F' {lang.name_en} Language Syntax'
 
         self.checked_fields = set()
         self.current_idx = 0
         self.current_batch_notes = []
 
-        self.setWindowTitle(F'Add {lang.name_en} language syntax')
+        window_title = F'Remove {lang.name_en} Language Syntax' if is_remove else F'Add {lang.name_en} Language Syntax'
+        self.setWindowTitle(window_title)
         self.setWindowIcon(util.default_icon())
 
         lyt = QVBoxLayout()
         self.setLayout(lyt)
 
-        lyt.addWidget(QLabel('Fields to add syntax to:'))
+        text = F'Fields to remove {lang.name_en} syntax from:' if is_remove else F'Fields to add {lang.name_en} syntax to:'
+        lyt.addWidget(QLabel(text))
 
         self.field_list = QListWidget()
         last_checked = config.get('syntax_fields_last_checked', {})
@@ -40,16 +68,20 @@ class AddSyntaxDialog(QDialog):
             self.field_list.addItem(itm)
         lyt.addWidget(self.field_list)
 
-        lyt.addWidget(ConnectionStatusLabel())
+        if not self.is_remove:
+            lyt.addWidget(ConnectionStatusLabel())
 
         button_box = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
-        button_box.accepted.connect(self.start_adding)
+        button_box.accepted.connect(self.start)
         button_box.rejected.connect(self.reject)
         lyt.addWidget(button_box)
+
+        self.resize(*self.INITIAL_SIZE)
 
 
     def accept(self):
         self.update_last_checked_config()
+        aqt.mw.reset()
         super().accept()
 
 
@@ -66,7 +98,7 @@ class AddSyntaxDialog(QDialog):
         config.set('syntax_fields_last_checked', last_checked, do_write=True)
 
 
-    def start_adding(self):
+    def start(self):
         self.checked_fields = set()
         for i in range(self.field_list.count()):
             itm = self.field_list.item(i)
@@ -74,12 +106,36 @@ class AddSyntaxDialog(QDialog):
             field_checked = itm.checkState() == Qt.Checked
             if field_checked:
                 self.checked_fields.add(field_name)
-        
-        self.current_idx = 0
-        aqt.mw.progress.start(min=0, max=len(self.note_ids), label='Adding Language Syntax', parent=self)
-        self.request_next_batch()
 
-    
+        aqt.mw.progress.start(
+            min=0,
+            max=self.note_count,
+            label= self.progress_string,
+            parent=self
+        )
+        
+        if self.is_remove:
+            self.remove_thread = self.RemoveThread(self.lang, self.note_ids, self.checked_fields)
+            self.remove_thread.finished.connect(self.on_finished)
+            self.remove_thread.progress.connect(self.on_progress)
+            self.remove_thread.start()
+
+        else:
+            self.current_idx = 0
+            self.request_next_batch()
+
+    def on_progress(self, progress):
+        aqt.mw.progress.update(
+            value=progress,
+            max=self.note_count,
+            label=self.progress_string + F'\n({progress}/{self.note_count})'
+        )
+
+    def on_finished(self):
+        aqt.mw.progress.finish()
+        self.accept()
+
+
     def request_next_batch(self):
         current_batch = self.note_ids[self.current_idx:self.current_idx + self.BATCH_SIZE]
         
@@ -117,12 +173,11 @@ class AddSyntaxDialog(QDialog):
         
         self.current_idx += current_batch_size
 
-        if self.current_idx < len(self.note_ids):
-            aqt.mw.progress.update(value=self.current_idx)
+        if self.current_idx < self.note_count:
+            self.on_progress(self.current_idx)
             self.request_next_batch()
         else:
-            aqt.mw.progress.finish()
-            self.accept()
+            self.on_finished()
 
     def on_batch_error(self, msg):
         aqt.mw.progress.clear()
@@ -130,7 +185,7 @@ class AddSyntaxDialog(QDialog):
 
 
     @classmethod
-    def show_modal(cls, note_ids, parent=None):
+    def show_modal(cls, note_ids, is_remove, parent=None):
 
         if note_ids is None or len(note_ids) < 1:
             return
@@ -150,7 +205,7 @@ class AddSyntaxDialog(QDialog):
                              F'If you want to set the language for to the "{note_type_name}" note type go to Tools > Manage Note Types > Migaku Options.')
             return
 
-        dlg = cls(lang, note_ids, parent)
+        dlg = cls(lang, is_remove, note_ids, parent)
         return dlg.exec_()
 
 
@@ -158,11 +213,17 @@ class AddSyntaxDialog(QDialog):
 def setup_browser_menu(browser: Browser):
     browser.form.menuEdit.addSeparator()
 
-    create_cards_action = QAction('Add language syntax to cards', browser)
-    create_cards_action.triggered.connect(
-        lambda: AddSyntaxDialog.show_modal(browser.selectedNotes(), browser)
+    add_syntax_action = QAction('Add language syntax to cards', browser)
+    add_syntax_action.triggered.connect(
+        lambda: AddRemoveSyntaxDialog.show_modal(browser.selectedNotes(), False, browser)
     )
-    browser.form.menuEdit.addAction(create_cards_action)
+    browser.form.menuEdit.addAction(add_syntax_action)
+
+    remove_syntax_action = QAction('Remove language syntax from cards', browser)
+    remove_syntax_action.triggered.connect(
+        lambda: AddRemoveSyntaxDialog.show_modal(browser.selectedNotes(), True, browser)
+    )
+    browser.form.menuEdit.addAction(remove_syntax_action)
 
 
 aqt.gui_hooks.browser_menus_did_init.append(setup_browser_menu)
