314803998a23a3527601ee54c08f9420c8aa8da6
Bent <bent@mail.de>
2021-12-04 21:50:46 +0100

    Don't remove spaces from unparsed japanese

--- src/languages/ja/__init__.py
+++ src/languages/ja/__init__.py
@@ -3,6 +3,8 @@ import re
 REMOVE_RE = re.compile(r'( +|\[(?!sound:).*?\])(?![^{]*})')
 
 def remove_syntax(text):
+    if not any(c in text for c in '{}[]'):
+        return text
     text = REMOVE_RE.sub('', text)
     text = text.replace('{', '')
     text = text.replace('}', '')
