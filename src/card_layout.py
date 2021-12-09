501189e1303a67e3ddd7117c3cc1722cd0e98aac
Bent <bent@mail.de>
2021-12-10 00:40:15 +0100

    Added ctrl key to override card layout hook

--- src/card_layout.py
+++ src/card_layout.py
@@ -10,7 +10,7 @@ from . import util
 def CardLayout_init_hook(self, mw: AnkiQt, note: Note, *args, **kwargs):
     if note:
         note_type = note.note_type()
-        if nt_was_installed(note_type):
+        if nt_was_installed(note_type) and not mw.app.queryKeyboardModifiers() & Qt.ControlModifier:
             parent = kwargs.get('parent', mw)
             util.show_info(
                 'The default Migaku note types cannot be edited because they are automatically updated and your changes would be lost.\n\n'
