d0955f8a2f7e8c4e093d8975f17a99372d061d02
Henrik Giesel <hengiesel@gmail.com>
2023-12-16 18:09:51 +0100

    fix: put exisiting or maybe non-existing patch arg into rest param

--- src/anki_version.py
+++ src/anki_version.py
@@ -34,7 +34,7 @@ def get_state() -> VersionState:
     except ValueError:
         return VersionState.UNKNOWN
 
-    major, minor = version_tuple
+    major, minor, *rest = version_tuple
 
     if major <= 2:
         # Old versioning scheme
