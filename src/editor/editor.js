64d08b05bfb25ad4e5c8c369f76b24b1962c7108
Henrik Giesel <hengiesel@gmail.com>
2023-12-26 00:12:07 +0100

    feat: add "First image" vs "Additional images"

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -6,25 +6,25 @@ function MigakuEditor() { }
  * @param {string} img_filter 
  */
 MigakuEditor.initButtons = async function (add_icon_path, remove_icon_path, img_filter) {
-  await new Promise((resolve) => setTimeout(resolve, 400))
+  await new Promise((resolve) => setTimeout(resolve, 400));
   document.querySelector('#migaku_btn_syntax_generate img').src = add_icon_path;
   document.querySelector('#migaku_btn_syntax_generate img').style.filter = img_filter;
   document.querySelector('#migaku_btn_syntax_remove img').src = remove_icon_path;
   document.querySelector('#migaku_btn_syntax_remove img').style.filter = img_filter;
   document.getElementById('migaku_btn_syntax_generate').style.display = '';
   document.getElementById('migaku_btn_syntax_remove').style.display = '';
-}
+};
 
 MigakuEditor.hideButtons = function () {
   if (!document.getElementById('migaku_btn_syntax_generate')) setTimeout(() => {
     document.getElementById('migaku_btn_syntax_generate').style.display = 'none';
     document.getElementById('migaku_btn_syntax_remove').style.display = 'none';
-  }, 100)
+  }, 100);
   else {
     document.getElementById('migaku_btn_syntax_generate').style.display = 'none';
     document.getElementById('migaku_btn_syntax_remove').style.display = 'none';
   }
-}
+};
 
 /** These are the values on CardFields */
 const selectorOptions = [
@@ -34,11 +34,13 @@ const selectorOptions = [
   { value: 'translation', text: 'Sentence Translation' },
   { value: 'sentenceAudio', text: 'Sentence Audio' },
   { value: 'wordAudio', text: 'Word Audio' },
-  { value: 'images', text: 'Image' },
+  { value: 'images', text: 'All Images' },
+  { value: 'firstImage', text: 'First Image' },
+  { value: 'restImages', text: 'Additional Images' },
   { value: 'definitions', text: 'Definitions' },
-  { value: 'exampleSentences', text: 'Example sentences' },
+  { value: 'exampleSentences', text: 'Example Sentences' },
   { value: 'notes', text: 'Notes' },
-]
+];
 
 function getSelectorField(editorField, settings) {
   const field = document.createElement('div');
@@ -55,16 +57,16 @@ function getSelectorField(editorField, settings) {
     select.append(optionElement);
   }
 
-  const fieldContainer = editorField.parentElement.parentElement
-  const labelName = fieldContainer.querySelector('.label-name').innerText
-  select.value = settings[labelName] ?? 'none'
+  const fieldContainer = editorField.parentElement.parentElement;
+  const labelName = fieldContainer.querySelector('.label-name').innerText;
+  select.value = settings[labelName] ?? 'none';
 
   select.addEventListener('change', (selectTarget) => {
-    const cmd = `migakuSelectChange:${selectTarget.currentTarget.value}:${labelName}`
-    bridgeCommand(cmd)
-  })
+    const cmd = `migakuSelectChange:${selectTarget.currentTarget.value}:${labelName}`;
+    bridgeCommand(cmd);
+  });
 
-  return field
+  return field;
 }
 
 const hiddenButtonCategories = [
@@ -74,7 +76,7 @@ const hiddenButtonCategories = [
   'template',
   'cloze',
   'image-occlusion-button',
-]
+];
 
 // New Migaku Editor
 function setupMigakuEditor(settings) {
@@ -83,12 +85,12 @@ function setupMigakuEditor(settings) {
   document.querySelectorAll('svg#mdi-pin-outline').forEach((field) => field.parentElement.parentElement.parentElement.style.display = 'none');
 
   hiddenButtonCategories.forEach((category) => {
-    const button = document.querySelector(`.item#${category}`)
-    if (button) button.style.display = 'none'
+    const button = document.querySelector(`.item#${category}`);
+    if (button) button.style.display = 'none';
   });
 
   for (const field of document.querySelectorAll('.editor-field')) {
-    field.append(getSelectorField(field, settings))
+    field.append(getSelectorField(field, settings));
   }
 }
 
@@ -98,12 +100,12 @@ MigakuEditor.resetMigakuEditor = function () {
   document.querySelectorAll('svg#mdi-pin-outline').forEach((field) => field.parentElement.parentElement.parentElement.style.display = '');
 
   hiddenButtonCategories.forEach((category) => {
-    const button = document.querySelector(`.item#${category}`)
-    if (button) button.style.display = ''
+    const button = document.querySelector(`.item#${category}`);
+    if (button) button.style.display = '';
   });
 
   document.querySelectorAll('.migaku-field-selector').forEach((selector) => selector.remove());
-}
+};
 
 MigakuEditor.toggleMode = function (settings) {
   if (document.querySelector('.migaku-field-selector')) {
@@ -111,32 +113,32 @@ MigakuEditor.toggleMode = function (settings) {
   } else {
     setupMigakuEditor(settings);
   }
-}
+};
 
 
 require('anki/ui')
   .loaded
   .then(() => new Promise((resolve) => setTimeout(resolve, 400)))
   .then(() => {
-    const migakuMode = document.getElementById('migaku_btn_toggle_mode')
-    if (!migakuMode) return
+    const migakuMode = document.getElementById('migaku_btn_toggle_mode');
+    if (!migakuMode) return;
 
-    const input = document.createElement('input')
-    input.type = 'checkbox'
-    input.id = 'migaku_btn_intercept_fields'
-    input.style.margin = '0 3px'
+    const input = document.createElement('input');
+    input.type = 'checkbox';
+    input.id = 'migaku_btn_intercept_fields';
+    input.style.margin = '0 3px';
 
-    const label = document.createElement('label')
-    label.htmlFor = 'migaku_btn_intercept_fields'
-    label.textContent = 'Intercept Fields'
-    label.style.userSelect = 'none'
-    label.style.padding = '0 3px'
+    const label = document.createElement('label');
+    label.htmlFor = 'migaku_btn_intercept_fields';
+    label.textContent = 'Intercept Fields';
+    label.style.userSelect = 'none';
+    label.style.padding = '0 3px';
 
     input.addEventListener('change', (e) => {
-      const cmd = `migakuIntercept:${e.currentTarget.checked}`
-      bridgeCommand(cmd)
-    })
+      const cmd = `migakuIntercept:${e.currentTarget.checked}`;
+      bridgeCommand(cmd);
+    });
 
-    migakuMode.parentElement.append(input)
-    migakuMode.parentElement.append(label)
-  })
+    migakuMode.parentElement.append(input);
+    migakuMode.parentElement.append(label);
+  });
